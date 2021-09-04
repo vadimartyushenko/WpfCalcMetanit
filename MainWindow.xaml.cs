b9fdfa1a2ecbb6422bb17a598e0bec9bd7357062
Вадим Артюшенко <artushenkomail@mail.ru>
2021-09-04 16:23:04 +0700

    add ToolTip and CheckBox

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows;
+using System.Windows.Media;
 using System.Windows.Controls;
 
 namespace WpfCalcMetanit
@@ -10,31 +10,48 @@ namespace WpfCalcMetanit
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		#region Private fields
+
 		private string _leftOper = "0";
 
 		private string _rightOper = "";
 
 		private string _operation = "";
 
+		#endregion
+
 		public MainWindow()
 		{
+
+			var gradientBrush = new LinearGradientBrush();
+			gradientBrush.GradientStops.Add(new GradientStop(Colors.LightGray, 0));
+			gradientBrush.GradientStops.Add(new GradientStop(Colors.Blue, 1));
+
+			this.Resources.Add("buttonGradientBrush", gradientBrush);
+			
 			InitializeComponent();
+
+			swhButton.Background = (Brush)TryFindResource("buttonGradientBrush");
+			escButton.Background = (Brush)TryFindResource("buttonGradientBrush");
 
 			txtBlock.Text += _leftOper;
 
 			foreach (UIElement childElement in LayoutRoot.Children)
 			{
 				if (childElement is Button)
-					((Button)childElement).Click += Button_Click;
+					((Button) childElement).Click += Button_Click;
 			}
+
+			escButton.Click += EscButton_Click;
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			var s = (string)((Button) e.OriginalSource).Content;
+			var s = (string) ((Button) e.OriginalSource).Content;
 
 			//TODO: доработать для случая расчета, например, 1+3*5 
-			 txtBlock.Text = txtBlock.Text =="0"? s : txtBlock.Text + s;
+			if (s.NotIn(Constants.ClearBtnSynonim, Constants.SwitchBtnSynonim))
+				txtBlock.Text = txtBlock.Text == "0" ? s : txtBlock.Text + s;
 
 			int numberBut;
 			var isNumber = Int32.TryParse(s, out numberBut);
@@ -42,7 +59,7 @@ namespace WpfCalcMetanit
 			if (isNumber)
 			{
 				if (string.IsNullOrWhiteSpace(_operation))
-					_leftOper =_leftOper == "0"? s: _leftOper+s;
+					_leftOper = _leftOper == "0" ? s : _leftOper + s;
 				else
 					_rightOper += s;
 			}
@@ -54,7 +71,7 @@ namespace WpfCalcMetanit
 					txtBlock.Text += _rightOper;
 					_operation = "";
 				}
-				else if (s == "CLEAR")
+				else if (s == Constants.ClearBtnSynonim)
 				{
 					_leftOper = "";
 					_rightOper = "";
@@ -69,6 +86,7 @@ namespace WpfCalcMetanit
 						_leftOper = _rightOper;
 						_rightOper = "";
 					}
+
 					_operation = s;
 				}
 
@@ -91,9 +109,9 @@ namespace WpfCalcMetanit
 				case "-":
 					_rightOper = (num1 - num2).ToString();
 					break;
-				
+
 				case "/":
-					_rightOper = ((double)num1 / num2).ToString();
+					_rightOper = ((double) num1 / num2).ToString();
 					break;
 
 				case "*":
@@ -104,7 +122,55 @@ namespace WpfCalcMetanit
 
 		private void SwhButton_OnClick(object sender, RoutedEventArgs e)
 		{
-			throw new NotImplementedException();
+			if (_operation != null)
+			{
+				txtBlock.Text = "";
+				_operation = "";
+				_rightOper = "";
+			}
+
+			_leftOper = _leftOper != null ? "-" + _leftOper : "-";
+			txtBlock.Text = _leftOper;
+
+			swhButton.Background = new SolidColorBrush(Colors.Blue);
+		}
+
+		private void EscButton_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
 		}
 	}
+
+	public static class Tools
+	{
+		public static bool In(this string str, params string[] items)
+		{
+			foreach (var i in items)
+				if (str == i)
+					return true;
+
+			return false;
+		}
+
+		public static bool NotIn(this string str, params string[] items)
+		{
+			foreach (var i in items)
+				if (str == i)
+					return false;
+
+			return true;
+		}
+	}
+
+	public static class Constants
+	{
+		#region Названия кнопок
+
+		public const string ClearBtnSynonim = "<CLEAR>";
+
+		public const string SwitchBtnSynonim = "Switch";
+
+		#endregion
+	}
+
 }
